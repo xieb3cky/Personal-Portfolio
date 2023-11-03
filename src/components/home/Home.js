b76620c798f49c542b3e0af86b4ecdd8dccf9c47
becky <xieb3cky@gmail.com>
2023-11-03 14:02:09 -0400

    added home base layout and styling

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './Home.css';
-
+import ImageOfMe from "../../assets/avatar-1.svg";
 export default function Home() {
     return (
-        <div>
+        <section className="home-container">
+            <div className="intro">
+                <img src={ImageOfMe} alt="" className="home-img" />
+                <h1 className="home-name">Becky Xie</h1>
+                <span className="home-education">
+                    I'm a Software Engineer!
+                </span>
 
-        </div>
+                <a href="#learn" className="learn-more btn">Learn More</a>
+            </div>
+        </section>
     )
 }
