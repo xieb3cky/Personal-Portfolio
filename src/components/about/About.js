4a5a52bfe3cb60eb06c9415279e90982cde05619
becky <xieb3cky@gmail.com>
2023-12-11 15:53:39 -0500

    update about me description

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -15,7 +15,7 @@ export default function About() {
                             Hello there! 👋 I'm a dynamic professional with a strong background in digital advertising and accounting.
                             As a graduate of the SpringBoard Software Engineering Program, I've seamlessly merged my skills in full-stack development, along with valuable experience in developing APIs, various programming languages, and frameworks.
                             My passion lies in utilizing technology to help others solve problems and enhance workflow efficiency.
-                            Let's connect and explore how my diverse skill set can contribute to meaningful and innovative solutions! <span className='learn-more-about'>Learn more! 👇</span>
+                            Let's connect and explore how my diverse skill set can contribute to meaningful and innovative solutions! <span className='learn-more-about'>Learn more below! 👇</span>
                         </p>
                         <a href="https://drive.google.com/file/d/1G0WBIt7hBDwy4TQvuzrmaqfXWlWwA8wb/view" className="btn" target="_blank" rel="noreferrer" >Download Resume </a>
                     </div>
