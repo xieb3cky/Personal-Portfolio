fb916877ba09153bd9d12c6f4f21a094f07fd6f7
becky <xieb3cky@gmail.com>
2023-12-11 12:44:39 -0500

    updated about me description

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -12,9 +12,10 @@ export default function About() {
                 <div className="about-data grid">
                     <div className="about-info">
                         <p className="about-description">
-                            Hi there! 👋 I have background in digital advertisting and accounting.
-                            I am skilled in full-stack development, project management, and client relationship building.
-                            My passion is utilizing technology to help others solve problems and improve workflow.  <span className='learn-more-about'>Learn more below ! 👇</span>
+                            Hello there! 👋 I'm a dynamic professional with a strong background in digital advertising and accounting.
+                            As a graduate of the SpringBoard Software Engineering Program, I've seamlessly merged my skills in full-stack development, along with valuable experience in developing APIs, various programming languages, and frameworks.
+                            My passion lies in utilizing technology to help others solve problems and enhance workflow efficiency.
+                            Let's connect and explore how my diverse skill set can contribute to meaningful and innovative solutions! <span className='learn-more-about'>Learn more! 👇</span>
                         </p>
                         <a href="https://drive.google.com/file/d/1G0WBIt7hBDwy4TQvuzrmaqfXWlWwA8wb/view" className="btn" target="_blank" rel="noreferrer" >Download Resume </a>
                     </div>
@@ -23,3 +24,6 @@ export default function About() {
         </section>
     )
 }
+
+
+
