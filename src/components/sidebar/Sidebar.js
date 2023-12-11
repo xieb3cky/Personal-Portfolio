5b5f7291de0ac6df27504847f25408b42862f5bf
becky <xieb3cky@gmail.com>
2023-12-11 10:20:54 -0500

    created sidebar component w/ styling

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,63 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
+import WomenIcon from '../../assets/woman.png';
 import './Sidebar.css';
-import { SlHome, SlUser, SlSocialLinkedin, SlSpeech, SlGrid, SlEmotsmile, SlBriefcase } from 'react-icons/sl';
+import { SlMenu } from 'react-icons/sl';
 import { TbCircleLetterB } from 'react-icons/tb';
-
+import { LiaHandPeace } from 'react-icons/lia';
+import { RiEmotionHappyLine, RiContactsBookLine, RiListIndefinite, RiBriefcaseLine, RiUser3Line, RiHomeSmile2Line } from "react-icons/ri";
 export default function Sidebar() {
+    const [menu, showMenu] = useState(false);
     return (
-        <aside className="aside">
-            <a href="#home" className="nav_logo">
-                {/* <img src="" alt="" srcset="" /> */}
-                <TbCircleLetterB />
-            </a>
-            <nav className="nav">
-                <div className="nav_menu">
-                    <ul className="nav_list">
-                        <li className="nav_item">
-                            <a href="#home" className="nav_link">
-                                <SlHome />
-                            </a>
-                        </li>
-                        <li className="nav_item">
-                            <a href="#About" className="nav_link">
-                                <SlUser />
-                            </a>
-                        </li>
-                        <li className="nav_item">
-                            <a href="#Experience" className="nav_link">
-                                <SlBriefcase />
-                            </a>
-                        </li>
-                        <li className="nav_item">
-                            <a href="#Projects" className="nav_link">
-                                <SlGrid />
-                            </a>
-                        </li>
-                        <li className="nav_item">
-                            <a href="#Socials" className="nav_link">
-                                <SlSocialLinkedin />
-                            </a>
-                        </li>
-                        <li className="nav_item">
-                            <a href="#FunFacts" className="nav_link">
-                                <SlEmotsmile />
-                            </a>
-                        </li>
-                        <li className="nav_item">
-                            <a href="#Contact" className="nav_link">
-                                <SlSpeech />
-                            </a>
-                        </li>
-                    </ul>
+        <>
+            <aside className={menu ? "aside show-menu" : "aside"}>
+                <a href="#Home" className='nav-logo'><img src={WomenIcon} alt="woman icon home" /></a>
+                <nav className="nav">
+                    <div className="nav_menu">
+                        <ul className="nav-list">
+                            <li className="nav_item">
+                                <a href="#Home" className="nav-link">
+                                    <RiHomeSmile2Line />
+                                </a>
+                            </li>
+                            <li className="nav_item">
+                                <a href="#About" className="nav-link">
+                                    <RiUser3Line />
+                                </a>
+                            </li>
+                            <li className="nav_item">
+                                <a href="#Experience" className="nav-link">
+                                    <RiBriefcaseLine />
+                                </a>
+                            </li>
+                            <li className="nav_item">
+                                <a href="#Projects" className="nav-link">
+                                    <RiListIndefinite />
+                                </a>
+                            </li>
+                            <li className="nav_item">
+                                <a href="#FunFacts" className="nav-link">
+                                    <RiEmotionHappyLine />
+                                </a>
+                            </li>
+                            <li className="nav_item">
+                                <a href="#Contact" className="nav-link">
+                                    <RiContactsBookLine />
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+
+                <div className="nav_footer">
+                    <span className="footer nav-link">
+                        <LiaHandPeace />
+                    </span>
                 </div>
-            </nav>
+            </aside >
 
-            <div className="nav_footer">
-                <span className="footer">
-                    text
-                </span>
+            <div className={menu ? "nav-toggle nav-toggle-open" : "nav-toggle"} onClick={() => showMenu(!menu)}>
+                <SlMenu />
             </div>
-        </aside>
 
+        </>
     )
 }
