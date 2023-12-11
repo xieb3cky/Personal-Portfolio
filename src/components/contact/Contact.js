2b0973fc8b844e7491dbed3069c4e204a4006ccb
becky <xieb3cky@gmail.com>
2023-12-11 10:19:19 -0500

    created contact me component w/ styling

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -3,8 +3,28 @@ import './Contact.css';
 
 export default function Contact() {
     return (
-        <div>
+        <section className="contact container section" id="Contact">
+            <h2 className="section-title">Get In Touch</h2>
 
-        </div>
+            <div className="contact-container grid">
+                <form action="" className="contact-form">
+                    <div className="contact-form-group">
+                        <div className="contact-form-div">
+                            <input type="text" className="contact-form-input" placeholder='Name' />
+                        </div>
+                        <div className="contact-form-div">
+                            <input type="email" className="contact-form-input" placeholder='Email' />
+                        </div>
+                    </div>
+                    <div className="contact-form-div">
+                        <input type="text" className="contact-form-input" placeholder='Subject Line' />
+                    </div>
+                    <div className="contact-form-div area">
+                        <textarea name="" id="" cols="30" rows="10" className="contact-form-input" placeholder='Message'></textarea>
+                    </div>
+                    <button className="btn">Send</button>
+                </form>
+            </div>
+        </section>
     )
 }
